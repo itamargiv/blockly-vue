6e668eecae542d32a2e7b6f546d3b6262014075a
Itamar Givon <itamar.givon.dev@gmail.com>
2019-10-01 16:49:29 +0200

    Add blockly component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Blockly id="blockly-editor">
+      <block type="controls_if"></block>
+      <block type="controls_repeat_ext"></block>
+      <block type="logic_compare"></block>
+      <block type="math_number"></block>
+      <block type="math_arithmetic"></block>
+      <block type="text"></block>
+      <block type="text_print"></block>
+    </Blockly>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Blockly from "./components/Blockly.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Blockly
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#blockly-editor {
+  height: 600px;
 }
 </style>
---
+++ src/components/Blockly.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="blockly-editor">
+    <div class="blockly-area" ref="area" :style="boxStyle"></div>
+    <xml class="blockly-toolbox" ref="toolbox" style="display:none">
+      <slot></slot>
+    </xml>
+  </div>
+</template>
+
+<script>
+import Blockly from "blockly";
+
+export default {
+  name: "Blockly",
+  data() {
+    return {
+      workspace: null,
+      boxStyle: null
+    };
+  },
+  mounted() {
+    this.workspace = Blockly.inject(this.$refs.area, {
+      toolbox: this.$refs.toolbox
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.blockly-editor {
+  position: relative;
+}
+
+.blockly-area {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>

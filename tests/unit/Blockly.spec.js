9b17e4d3e42808653dd46a928cd9f3ab1569848c
Itamar Givon <itamar.givon.dev@gmail.com>
2019-10-20 13:23:56 +0200

    Disable testing

---
+++ tests/unit/Blockly.spec.js
@@ -0,0 +1,14 @@
+/**
+ * TODO: Find a way to mock Blockly and Fetch for testing
+ */
+
+// import { shallowMount } from '@vue/test-utils';
+// import Blockly from '@/components/Blockly.vue';
+
+// describe('Blockly.vue', () => {
+//   it('renders a Blockly workspace');
+//   it('accepts and fetches a toolbox url');
+//   it('accepts and applies a toolbox');
+//   it('accepts and applies options');
+//   it('emits change events');
+// });

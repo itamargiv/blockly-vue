03da2939c91f92ed8de08c180258d238459a92e9
Itamar Givon <itamar.givon.dev@gmail.com>
2019-10-22 02:02:23 +0200

    Fix tests

--- tests/unit/Blockly.spec.js
+++ tests/unit/Blockly.spec.js
@@ -5,10 +5,10 @@
 // import { shallowMount } from '@vue/test-utils';
 // import Blockly from '@/components/Blockly.vue';
 
-// describe('Blockly.vue', () => {
-//   it('renders a Blockly workspace');
-//   it('accepts and fetches a toolbox url');
-//   it('accepts and applies a toolbox');
-//   it('accepts and applies options');
-//   it('emits change events');
-// });
+xdescribe('Blockly.vue', () => {
+  it('renders a Blockly workspace');
+  it('accepts and fetches a toolbox url');
+  it('accepts and applies a toolbox');
+  it('accepts and applies options');
+  it('emits change events');
+});
